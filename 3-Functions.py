08d8271165018217abaf0eae6ca5ad42fec5e3d1
EmucLaptop <owniigame@gmail.com>
2021-10-01 15:52:21 +0200

    more loc py refrsh and git check

---
+++ 3-Functions.py
@@ -0,0 +1,26 @@
+import random, sys, os, math
+import pyperclip
+
+def main():
+    print('Hello')
+    print(random.randint(1,10))
+    pyperclip.paste()
+
+
+
+
+
+
+
+
+
+#this is a script
+if __name__ == '__main__':
+    main()
+
+
+
+#Cant use multiprocessng without if name main idiom
+#Faster exec time with functions
+#Just remember it being like c++
+#   int main(), void main() etc.
---
+++ 39-Functions.py
@@ -0,0 +1,37 @@
+#write ur own function.
+import random
+
+
+# def hello():
+#     print('Howdy')
+#     print('Howdy!!!')
+#     print('Helo ther')
+
+
+# def hello(name):
+#     print('Hello ' + name)
+
+
+def plusOne(number):
+    return number + 1
+
+
+
+def main():
+    print('a','b', sep='DBD')
+    print('Hello')
+    print(random.randint(1,10))
+    newNumber = plusOne(5)
+    print(newNumber)
+
+
+
+
+
+
+
+
+#this is a script
+if __name__ == '__main__':
+    main()
+
