67dfc4843493418ad9377ede66321b5c6b11cfa8
EmucLaptop <owniigame@gmail.com>
2021-10-01 13:20:03 +0200

    more loc py refrsh and git check

--- sectionTwo.py
+++ sectionTwo.py
@@ -1,3 +1,7 @@
+## FLOW CONTROL ##
+
+## IF STATEMENTS ##
+
 # name = 'Bob'
 # if name == 'Alice':
 #     print(name)
@@ -11,14 +15,44 @@
 # else:
 #     print('wrong password')
 
-name = 'Bob'
-age = 3000
-if name == 'Alice':
-    print('Hi alice')
+# name = 'Bob'
+# age = 3000
+# if name == 'Alice':
+#     print('Hi alice')
+#
+# elif age < 12:
+#     print('You are not Alice')
+# elif age > 2000:
+#     print('Unlike you,,,')
+# elif age > 100:
+#     print('NotAlice')
 
-elif age < 12:
-    print('You are not Alice')
-elif age > 2000:
-    print('Unlike you,,,')
-elif age > 100:
-    print('NotAlice')
+
+## WHILE LOOPS ##
+
+# spam = 0
+# while spam < 5:
+#     print('Hi')
+#     spam = spam+1
+
+
+# name = ''
+# while name != 'your name':
+#     print('Please type your name.')
+#     name = input()
+#
+# print('Thank You!')
+
+##use breaks whenever necessary to exit infinite loops
+
+# spam = 0
+# while spam < 5:
+#     spam = spam + 1
+#     if spam == 3:
+#         continue
+#     print('Spam is: ' + str(spam))
+#     #continue we restart the loop
+
+
+### FOR LOOPS ###
+
