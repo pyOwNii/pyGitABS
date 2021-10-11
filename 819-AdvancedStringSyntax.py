c4bf1b89ca22afbc46c0022327af501e352b664f
EmucLaptop <owniigame@gmail.com>
2021-10-11 20:03:04 +0200

    S8.
    Private repo test

---
+++ 819-AdvancedStringSyntax.py
@@ -0,0 +1,13 @@
+def main():
+    # using double " quotes makes it that u can use single quotes in it.
+    print("Hi")
+    print('Say hi to bo\'s test\n')
+
+    print('hello there.\nhow are u?\ntest')
+
+    #raw string
+    print(r'Hello')
+    print(r'That is a cat')
+
+if __name__ == '__main__':
+    main()
---
+++ 821-StringFormatting.py
@@ -0,0 +1,7 @@
+def main():
+    #using double " quotes makes it that u can use single quotes in it.
+    print("Hi")
+
+
+if __name__ == '__main__':
+    main()
