46340b0a5aa20dee94055abb9e26ae6c1d626861
EmucLaptop <owniigame@gmail.com>
2021-10-20 14:16:09 +0200

    S8.
    Private repo test

--- 820-StringMethods.py
+++ 820-StringMethods.py
@@ -3,12 +3,55 @@ def main():
     print(test.upper())
     print(test.lower())
 
-    answer = input()
+    #answer = input()
 
-    if answer.lower() == 'yes':
-        print('okay')
-    else:
-        print('not ok')
+    # if answer.lower() == 'yes':
+    #     print('okay')
+    # else:
+    #     print('not ok')
+    #
+
+    print(test.islower())
+
+    print("hello".upper().isupper())
+
+    #.isalpha ## letters
+    #.isalnum() ## letters and numerics
+    #.isdecimal()
+    #.isspace()
+
+    #.istitle() #title
+
+    print("hello world!".startswith("hello"))
+    print("hello world".endswith("world"))
+
+    #join
+    print(', '.join(['cats', 'rats', 'bats']))
+    print(''.join(['cats', 'rats', 'bats']))
+    print(' '.join(['cats', 'rats', 'bats']))
+    print('\n'.join(['cats', 'rats', 'bats']))
+
+    #split
+    print("my name is hello".split())
+    print("my name is hello".split("m"))
+
+    #ljust rjust
+    print("hello".rjust(10))
+    print("hello".ljust(10))
+    print("hello".rjust(20, "*"))
+    print("hello".center(20, "="))
+
+    name = 'Test'
+    print(name.center(20, '"'))
+
+    #strip to remove
+
+    spam = "aaaarrraaaarrraaaa"
+    print(spam.strip("aaaa"))   #removes the other aaaas
+
+    #replace
+    spamx = 'Hello there'
+    print(spamx.replace('e', 'XYZ'))
 
 if __name__ == '__main__':
     main()
--- 821-StringFormatting.py
+++ 821-StringFormatting.py
@@ -1,7 +1,17 @@
+
 def main():
     #using double " quotes makes it that u can use single quotes in it.
-    print("Hi")
+    xyz = 'hello ' + 'world'
+    name = 'Alice'
+    place = 'Main Street'
+    time = '6 pm'
+    food = 'turnips'
 
+    sentence = 'Hello ' + name + ', you are invited to a party at ' + place + ' at ' + time + '. Please ' + food + '.'
+    print(sentence)
+
+    sentence2 = 'Hello %s, your are invite to a party at %s at %s. Please bring %s.' %(name, place, time, food)
+    print(sentence2)
 
 if __name__ == '__main__':
     main()
